804f8fa09e1c1f0735409f136519ac6dee702e50
doccodyblue <doccodyblue@gmail.com>
2026-09-14 17:45:29 +0200

    Cut 0.16.0

--- pvstrings-dash.js
+++ pvstrings-dash.js
@@ -27,7 +27,7 @@
 
 /* ============================ SECTION: HEADER ============================ */
 
-const PVS_VERSION = "0.15.1";
+const PVS_VERSION = "0.16.0";
 const PVS_MIN_INTEGRATION = "1.8.0";
 
 /* ============================ SECTION: CONST ============================= */
